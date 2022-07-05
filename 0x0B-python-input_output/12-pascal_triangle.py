57485a17330566e5af610a41aef1856abf76df17
steve99-coder <stevenderitu99@gmail.com>
2022-07-05 21:02:04 +0000

    changes

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -27,4 +27,5 @@ def pascal_triangle(n):
     for rows in range(n-1):
         triangle.append([a+b for a, b
                          in zip([0] + triangle[-1], triangle[-1] + [0])])
-    return 
+    return triangle
+
