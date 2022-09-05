44c66b7e66465705f405fa1162e8f948a9b8de84
steve99-coder <stevenderitu99@gmail.com>
2022-09-05 22:02:40 +0000

    Add files

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
+// Script that prints 3 lines.
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
 
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+lines.forEach(print);
+
+function print (line) {
+  console.log(line);
+}
