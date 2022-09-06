7f9fbf26ad20b7812c599f5dccee338b6655a200
steve99-coder <stevenderitu99@gmail.com>
2022-09-07 01:17:07 +0300

    Add files

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-let index = 0;
+let count = 0;
 
-/**
- * logMe - prints the number of arguments already printef and the new argument value
- * @params item
- */
 exports.logMe = function (item) {
-  console.log(`${index++}: ${item}`);
-}
+  console.log(count + ': ' + item);
+  count++;
+};
