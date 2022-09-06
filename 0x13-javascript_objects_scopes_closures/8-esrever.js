75b33637634e29a4e5f14b92012d359ba8fcd06d
steve99-coder <stevenderitu99@gmail.com>
2022-09-07 01:11:28 +0300

    Add files

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,17 +1,7 @@
 #!/usr/bin/node
-/**
- * esrever - returns the reversed version of a list.
- * @params list
- */
+
 exports.esrever = function (list) {
-  let beg = 0;
-  let end = list.length - 1;
-  while (beg < end) {
-    let tmp = list[beg];
-    list[beg] = list[end];
-    list[end] = tmp;
-    beg++;
-    end--;
-  }
-  return list;
+  const listCopy = [];
+  list.forEach(element => listCopy.unshift(element));
+  return listCopy;
 };
